4996a9c23d9bd4691bbfbe691f8eb629c23237ce
Yash-11111 <yashkesarwani8040@gmail.com>
2025-08-04 11:35:38 +0530

    Initial commit multiplication file

---
+++ mul.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class mul{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number one");
+        int a = sc.nextInt();
+        System.out.println("Enter number two");
+        int b = sc.nextInt();
+
+
+        System.out.println("multiplication of two given number is " + (a*b));
+
+    }
+}
