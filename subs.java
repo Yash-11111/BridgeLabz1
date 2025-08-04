1335ae8491103fb3157c8ec10ec9c6b70abbb8d4
Yash-11111 <yashkesarwani8040@gmail.com>
2025-08-04 11:31:02 +0530

    Initial commit subs

---
+++ subs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class subs{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number one");
+        int a = sc.nextInt();
+        System.out.println("Enter number two");
+        int b = sc.nextInt();
+
+
+        System.out.println("addition of two given number is " + (a-b));
+
+    }
+}
